f199b7710d13de4a8df5bed0fde1d997b869cd53
candy gilles <poundensigue@gmail.com>
2023-03-27 19:44:59 -0400

    nav bar finished

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,33 @@
-<div class="bg-gray-500 flex justify-center items-center h-screen ">
-	<h1 class="font-bold text-5xl text-white">Here the magic start</h1>
-</div>
+<script>
+    import NavBar from "$lib/navbar.svelte"
+</script>
+
+<NavBar/>
+<section>
+    <div class="px-10">
+        <img src="images/image-product-1.jpg" class="w-2/6 rounded-lg" alt="">
+        <div>
+            <img src="images/image-product-1-thumbnail.jpg" alt="">
+            <img src="images/image-product-2-thumbnail.jpg" alt="">
+            <img src="images/image-product-3-thumbnail.jpg" alt="">
+            <img src="images/image-product-4-thumbnail.jpg" alt="">
+        </div>
+    </div>
+    <div>
+        <p>sneaker company</p>
+        <h1>Fall Limited Edition Sneakers</h1>
+        <p>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole,
+            they’ll withstand everything the weather can offer.</p>
+        <div>
+            <p>$125.00</p>
+        </div>
+        <div>
+            <div>
+                <img src="images/icon-minus.svg" alt="">
+                <span>0</span>
+                <img src="images/icon-plus.svg" alt="">
+            </div>
+            <button><img src="images/icon-cart.svg"> Add to cart</button>
+        </div>
+    </div>
+</section>
